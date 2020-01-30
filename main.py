64f6498f8bf8c7b5c842ba9514b3249d336bc93b
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-01-30 20:16:58 +0000

    added a run button

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from functools import partial
+
+
 
 class FirstKivy(App): 
+    def runScript(self,instance,*args):
+        instance.text = "test button"
     def build(self):
-        return Button(text="Start Pomodore Timer!")
+        runBtn = Button(text="Start Pomodore Timer!")
+        runBtn.bind(on_press = partial(self.runScript,runBtn))
+        return runBtn
+    
 
 FirstKivy().run()
